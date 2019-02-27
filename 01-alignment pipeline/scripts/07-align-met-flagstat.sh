ce31ea6f3b76addefc210da959a2f075a8508744
mattsada <mattsada@gmail.com>
2019-02-27 10:47:27 -0800

    Update 07-align-met-flagstat.sh

--- 01-alignment pipeline/scripts/07-align-met-flagstat.sh
+++ 01-alignment pipeline/scripts/07-align-met-flagstat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for sample in `ls DATA/mdup*.bam`
+for sample in `ls ./DATA/mdup*.bam`
 do
-dir="/DATA/mdup"
-out="/metrics/flagstat/"
+dir="./DATA/mdup"
+out="./metrics/flagstat/"
 base=$(basename $sample ".bam")
 samtools flagstat ${dir}/${base}.bam > ${out}/${base}.flagstat.txt
 done
