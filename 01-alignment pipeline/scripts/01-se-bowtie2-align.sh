db9bbadbe88bcb28515e172e4f9e590e7cd3a886
mattsada <mattsada@gmail.com>
2019-02-27 10:45:39 -0800

    Update 01-se-bowtie2-align.sh

--- 01-alignment pipeline/scripts/01-se-bowtie2-align.sh
+++ 01-alignment pipeline/scripts/01-se-bowtie2-align.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for sample in `ls /DATA/fastq/SE/*.fastq`
+for sample in `ls ./DATA/fastq/SE/*.fastq`
 do
-dir="/DATA/fastq/SE/"
+dir="./DATA/fastq/SE/"
 base=$(basename $sample ".fastq")
-out="/DATA/sam"
+out="./DATA/sam"
 bowtie2 -x 'refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index' -U ${dir}/${base}.fastq -S ${out}/${base}.sam
 done
