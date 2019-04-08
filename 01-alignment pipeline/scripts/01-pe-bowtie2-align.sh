3b6cc20d346ef2f691998ebba5b8ecc38842ba40
mattsada <mattsada@gmail.com>
2019-04-08 09:33:22 -0700

    Update 01-pe-bowtie2-align.sh

--- 01-alignment pipeline/scripts/01-pe-bowtie2-align.sh
+++ 01-alignment pipeline/scripts/01-pe-bowtie2-align.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for sample in `ls /media/mattsada/mattsada_2TB/DATA/HGSV/CHS/HG00514/*1.fastq.gz`
+for sample in `ls ./DATA/fastq/PE/*1.fastq.gz`
 do
-dir="/media/mattsada/mattsada_2TB/DATA/HGSV/CHS/HG00514/"
+dir="./DATA/fastq/PE/"
 base=$(basename $sample "_1.fastq.gz")
 out="./DATA/sam/"
 bowtie2 -x 'refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index' -1 ${dir}/${base}_1.fastq.gz -2 ${dir}/${base}_2.fastq.gz -S ${out}/${base}.sam
