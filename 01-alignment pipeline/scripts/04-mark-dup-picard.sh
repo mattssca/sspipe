6908dae4ecc79947c38d589aa8f0d2dfd2c04499
mattsada <mattsada@gmail.com>
2019-04-08 12:18:47 -0700

    Update 04-mark-dup-picard.sh

--- 01-alignment pipeline/scripts/04-mark-dup-picard.sh
+++ 01-alignment pipeline/scripts/04-mark-dup-picard.sh
@@ -8,6 +8,6 @@ out="./metrics/mdup/"
 base=$(basename $sample ".bam")
 java -jar '/home/mattsada/packages/picard-2.18.11/picard.jar' MarkDuplicates \
 I= ${dir}/${base}.bam \
-O= ${dirout}/${base}.mdup \
+O= ${dirout}/${base}.mdup.bam \
 M= ${out}/${base}.mdup.txt
 done
