42afa32238d538fe7c43e34f9f422354f4eb4b81
mattsada <mattsada@gmail.com>
2019-04-23 11:42:27 -0700

    moving files

--- 01-alignment pipeline/scripts/08-insert-size-picard.sh
+++ 01-alignment pipeline/scripts/08-insert-size-picard.sh
@@ -4,10 +4,11 @@ for sample in `ls ./DATA/mdup/*.bam`
 do
 dir="./DATA/mdup/"
 out="./metrics/CollectInsertSizeMetrics/"
+out2="./figs/CollectInsertSizeMetrics/"
 base=$(basename $sample ".bam")
-java -jar '/Users/mattsada/packages/picard/build/libs/picard.jar' picard.jar CollectInsertSizeMetrics \
+java -jar '/Users/mattsada/packages/picard/build/libs/picard.jar' CollectInsertSizeMetrics \
 I=${dir}/${base}.bam \
 O=${out}/${base}.colinsert.txt \
-H=${out}/${base}.insert_size_histogram.pdf \
+H=${out2}/${base}.insert_size_histogram.pdf \
 M=0.5
 done
