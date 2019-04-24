f57efcf75b2619e19c5656d4f1dc015ca4b04f62
mattsada <mattsada@gmail.com>
2019-04-24 11:36:54 -0700

    Add files via upload

---
+++ 01-alignment pipeline/scripts/09-gc-bias-picard.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for sample in `ls ./DATA/mdup/*.bam`
+do
+dir="./DATA/mdup/"
+out="./metrics/CollectGCBiasMetrics/"
+out2="./figs/CollectGCBiasMetrics/"
+base=$(basename $sample ".bam")
+java -jar picard.jar CollectGcBiasMetrics \
+I=${dir}/${base}.bam \
+O=${out}/${base}.gc_bias_metrics.txt \
+CHART=${out2}/${base}gc_bias_metrics.pdf \
+S=${out}/${base}.summary_metrics.txt \
+R=./refseq/hg38.fa \
+done
