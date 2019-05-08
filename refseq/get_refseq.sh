aa0e3a7b8c908ab44cdc41c24208b373e41da0ba
mattsada <mattsada@gmail.com>
2019-05-08 16:14:39 -0400

    Update get_refseq.sh

--- refseq/get_refseq.sh
+++ refseq/get_refseq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#cd sspipe-master before executing script
+
 #download Bowtie2 index refseq
 wget http://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz -P ./refseq 
 gunzip ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
