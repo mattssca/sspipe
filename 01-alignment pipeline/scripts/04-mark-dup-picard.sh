079bfe7d61909c3bedcff9739dc5b5f611338ba5
mattsada <mattsada@gmail.com>
2019-05-10 20:08:12 -0400

    deflater issue

--- 01-alignment pipeline/scripts/04-mark-dup-picard.sh
+++ 01-alignment pipeline/scripts/04-mark-dup-picard.sh
@@ -8,6 +8,7 @@ out="./metrics/mdup/"
 base=$(basename $sample ".bam")
 java -jar '/home/mattsada/packages/picard-2.18.11/picard.jar' MarkDuplicates \
 I= ${dir}/${base}.bam \
+#USE_JDK_DEFLATER=TRUE \ #ixes deflater java related error on macos
 O= ${dirout}/${base}.mdup.bam \
 M= ${out}/${base}.mdup.txt
 done
