530be33de8566620e2af40afbbefaeb59f8ad1a9
mattsada <mattsada@gmail.com>
2019-05-17 09:43:26 -0700

    updating master script to include r scripts

--- 01-alignment pipeline/master.sh
+++ 01-alignment pipeline/master.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
 ./01-alignment\ pipeline/scripts/01-pe-bowtie2-align.sh
-echo "01-pe-bowtie2-align.sh executed succesfully"
 ./01-alignment\ pipeline/scripts/02-sam2bam.sh
-echo "02-sam2bam.sh executed succesfully"
-rm -rf ./DATA/sam/*
 ./01-alignment\ pipeline/scripts/03-bamsort.sh
-echo "03-bamsort.sh executed succesfully"
-rm -rf ./DATA/bam/*
 ./01-alignment\ pipeline/scripts/04-mark-dup-picard.sh
-echo "04-mark-dup-picard.sh executed succesfully"
-rm -rf ./DATA/sorted/*
 ./01-alignment\ pipeline/scripts/05-index.sh
-echo "05-index.sh executed succesfully"
 ./01-alignment\ pipeline/scripts/06-align-met-picard.sh
-echo "06-align-met-picard.sh executed succesfully"
 ./01-alignment\ pipeline/scripts/07-align-met-flagstat.sh
-echo "07-align-met-flagstat.sh executed succesfully"
 ./01-alignment\ pipeline/scripts/08-insert-size-picard.sh
-echo "08-insert-size-picard.sh executed succesfully"
-./01-alignment\ pipeline/scripts/09-gc-bias-picard.sh
-echo "09-gc-content.sh executed succesfully"
-./01-alignment\ pipeline/scripts/10-BAIT.sh
-echo "10-BAIT.sh executed succesfully"
+Rscript ./01-alignment\ pipeline/scripts/10-dataman.R
+Rscript ./01-alignment\ pipeline/scripts/11-datavis.R
+./01-alignment\ pipeline/scripts/12-BAIT.sh
