e9b7738217da67d3aa8b65b31a6e692644b5274f
mattsada <mattsada@gmail.com>
2019-05-22 13:14:08 -0700

    Delete 02-catROI.sh

--- 02-inversion analysis pipeline/scripts/02-catROI.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#cd directory with all ROI files
-cd "/home/mattsada/Desktop/invref/ROI/"
-sudo cat *txt >> ROI_locations_allChr.txt 
-
-#delete old individual ROI files
-rm ROI_locations_Table_b50_chr{1..22}.txt
-rm ROI_locations_Table_b50_chrX.txt
