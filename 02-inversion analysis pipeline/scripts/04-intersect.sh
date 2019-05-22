b292f6b798d9aa07e230317608277ad76d20e5d0
mattsada <mattsada@gmail.com>
2019-05-22 13:14:27 -0700

    Delete 04-intersect.sh

--- 02-inversion analysis pipeline/scripts/04-intersect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- bedtools intersect -a '/home/mattsada/Desktop/invref/bed/ROI_NAWC.bed' \
-  -b '/home/mattsada/Desktop/invref/bed/ROI_AWC.bed' \
-  -v \
-  > '/home/mattsada/Desktop/invref/bed/ROI_intersect.bed'
