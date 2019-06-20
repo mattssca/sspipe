7794405808c29e23db20fcd789e10245f7d63c99
mattsada <mattsada@gmail.com>
2019-06-20 14:07:36 -0700

    fixed tar issue

--- refseq/get_refseq.sh
+++ refseq/get_refseq.sh
@@ -4,7 +4,7 @@
 
 #download Bowtie2 index refseq
 wget http://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz -P ./refseq 
-gunzip ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
+tar -zxvf ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
 
 #download refseq in FASTA format
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P ./refseq 
@@ -12,4 +12,3 @@ gunzip ./refseq/hg38.fa.gz
 
 #convert bowtie2 formated reference genome back to FASTA for CollectGCBiasMetrics
 bowtie2-inspect './refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index' > ./refseq/bowtie2.GCHr38.fasta
-
