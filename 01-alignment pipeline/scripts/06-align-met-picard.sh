b323464788f514230d134f3d6f5d2366cea82490
mattsada <mattsada@gmail.com>
2019-06-20 17:12:39 -0700

    updating reference genome

--- 01-alignment pipeline/scripts/06-align-met-picard.sh
+++ 01-alignment pipeline/scripts/06-align-met-picard.sh
@@ -6,7 +6,7 @@ dir="./DATA/mdup/"
 out="./metrics/CollectAlignmentMetrics/"
 base=$(basename $sample ".bam")
 java -jar '/home/mattsada/packages/picard-2.18.11/picard.jar' CollectAlignmentSummaryMetrics \
-R=./refseq/hg38.fa \
+R=./refseq/GRCh38.fasta \
 I=${dir}/${base}.bam \
 O=${out}/${base}.colalmet.txt
 done
