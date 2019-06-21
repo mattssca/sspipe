b348cd800d8882edf2e013b7475bf1eec60c0577
mattsada <mattsada@gmail.com>
2019-06-20 17:11:40 -0700

    fix path

--- refseq/get_refseq.sh
+++ refseq/get_refseq.sh
@@ -4,11 +4,8 @@
 
 #download Bowtie2 index refseq
 wget http://ftp.ncbi.nlm.nih.gov/genomes/archive/old_genbank/Eukaryotes/vertebrates_mammals/Homo_sapiens/GRCh38/seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz -P ./refseq 
-tar -zxvf ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
+tar -zxvf ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz -C ./refseq/
+rm ./refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index.tar.gz
 
-#download refseq in FASTA format
-wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz -P ./refseq 
-gunzip ./refseq/hg38.fa.gz
-
-#convert bowtie2 formated reference genome back to FASTA for CollectGCBiasMetrics
-bowtie2-inspect './refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index' > ./refseq/bowtie2.GCHr38.fasta
+#convert bowtie2 formated reference genome back to FASTA for CollectGCBiasMetrics and other qc
+bowtie2-inspect './refseq/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index' > ./refseq/GRCh38.fasta
