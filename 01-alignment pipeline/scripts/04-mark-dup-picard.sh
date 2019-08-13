28fd838c5776cf14f8f5b8da0851f886f80fda1c
mattsada <mattsada@gmail.com>
2019-08-13 11:01:58 -0700

    Update 04-mark-dup-picard.sh
    updating path to picard

--- 01-alignment pipeline/scripts/04-mark-dup-picard.sh
+++ 01-alignment pipeline/scripts/04-mark-dup-picard.sh
@@ -6,7 +6,7 @@ dir="./DATA/sorted/"
 dirout="./DATA/mdup/"
 out="./metrics/mdup/"
 base=$(basename $sample ".bam")
-java -jar '/home/mattsada/packages/picard-2.18.11/picard.jar' MarkDuplicates \
+picard MarkDuplicates \
 I= ${dir}/${base}.bam \
 #USE_JDK_DEFLATER=TRUE fiixes deflater java related error on macos
 O= ${dirout}/${base}.mdup.bam \
