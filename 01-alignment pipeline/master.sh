dbd4f133c45eb60cfc402905a34fd13bab655a65
mattsada <mattsada@gmail.com>
2019-08-21 11:36:46 -0700

    Update master.sh

--- 01-alignment pipeline/master.sh
+++ 01-alignment pipeline/master.sh
@@ -9,8 +9,9 @@
 ./01-alignment\ pipeline/scripts/07-align-met-flagstat.sh
 ./01-alignment\ pipeline/scripts/08-insert-size-picard.sh
 ./01-alignment\ pipeline/scripts/09-gc-bias-picard.sh
-./01-alignment\ pipeline/scripts/10-trim-samtools.sh
-./01-alignment\ pipeline/scripts/11-gccov-bedtools.sh
-Rscript ./01-alignment\ pipeline/scripts/12-dataman.R
-Rscript ./01-alignment\ pipeline/scripts/13-datavis.R
-./01-alignment\ pipeline/scripts/14-BAIT.sh
+#./01-alignment\ pipeline/scripts/10-trim-samtools.sh
+#./01-alignment\ pipeline/scripts/11-gccov-bedtools.sh
+# Rscript ./01-alignment\ pipeline/scripts/12-gc-cov-plot.R
+Rscript ./01-alignment\ pipeline/scripts/13-dataman.R
+Rscript ./01-alignment\ pipeline/scripts/14-datavis.R
+#./01-alignment\ pipeline/scripts/15-BAIT.sh
