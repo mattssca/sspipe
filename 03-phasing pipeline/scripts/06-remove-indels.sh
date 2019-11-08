a5bdfe06204cf704ef5e300ecdb1bf253d8b49fc
mattsada <mattsada@gmail.com>
2019-11-08 12:38:14 -0800

    Update 06-remove-indels.sh

--- 03-phasing pipeline/scripts/06-remove-indels.sh
+++ 03-phasing pipeline/scripts/06-remove-indels.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-#remove indels (keep SNVs) entries from SV callset
-vcftools --vcf ./03-phasing\ pipeline/dependencies/SNV.vcf  --remove-indels --recode --recode-INFO-all --out ./03-phasing\ pipeline/dependencies/SNV-noindel.bed 
+#remove indels (keep SNVs) entries from input callset
+vcftools --vcf ./03-phasing\ pipeline/dependencies/SNV-man.bed  --remove-indels --recode --recode-INFO-all --out ./03-phasing\ pipeline/dependencies/SNV-noindel.bed 
+
+#removes SNVs (keep indels) entries from input callset
+vcftools --vcf ./03-phasing\ pipeline/dependencies/SNV-man.bed --keep-indels --recode --recode-INFO-all --out ./03-phasing\ pipeline/dependencies/SNV-noindel.bed 
