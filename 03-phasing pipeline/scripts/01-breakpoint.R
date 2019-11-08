591fab7b974e6df7c8c08d014b99c8f622068e1d
mattsada <mattsada@gmail.com>
2019-11-08 11:56:13 -0800

    updating paths

--- 03-phasing pipeline/scripts/01-breakpoint.R
+++ 03-phasing pipeline/scripts/01-breakpoint.R
@@ -1,30 +1,8 @@
-#github install
-library(devtools)
-install_github("daewoooo/breakpointRdata")
-install_github("daewoooo/breakpointR")
-
-#biconductor install
-install.packages("BiocManager")
-BiocManager::install("breakpointR", version = "3.8")
-
 library(breakpointR)
 
-#se libraries
-breakpointr(inputfolder = "../mattsada/Desktop/tmp/se/", 
-            outputfolder = "../mattsada/Desktop/tmp/output/", 
-            chromosomes = paste0 ("chr", c(1:22)), 
-            pairedEndReads = FALSE, 
-            reuse.existing.files = FALSE, 
-            windowsize = 1000000, 
-            binMethod = 'size', 
-            pair2frgm = FALSE, 
-            min.mapq = 10, 
-            filtAlt = TRUE, 
-            callHotSpots=TRUE)
-
-#pe libraries
-breakpointr(inputfolder = "../mattsada/Desktop/tmp/pe/", 
-            outputfolder = "../mattsada/Desktop/tmp/pe-output/", 
+#pe strand-seq libraries
+breakpointr(inputfolder = "./DATA/trimmed/", 
+            outputfolder = "..DATA/breakpointR/", 
             chromosomes = paste0 ("chr", c(1:22)), 
             pairedEndReads = TRUE, 
             reuse.existing.files = FALSE, 
