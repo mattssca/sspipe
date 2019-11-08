e853fd6dd00369fea09917ce1f82d58a7b6273a1
mattsada <mattsada@gmail.com>
2019-11-08 12:13:12 -0800

    Update and rename 03-pe-strandphase.R to 04-strandphase.R

---
+++ 03-phasing pipeline/scripts/04-strandphase.R
@@ -0,0 +1,19 @@
+#how to aquire StrandPhaseR package
+#library(devtools) 
+#install_git("git://github.com/daewoooo/StrandPhaseR.git", branch = "devel")
+#BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
+
+# load packages
+library(StrandPhaseR)
+library(BSgenome.Hsapiens.UCSC.hg38)
+
+#run strandphaseR: 
+strandPhaseR(inputfolder = "./DATA/trimmed/", 
+             outputfolder = "/03-phasing\ pipeline/output/strandphaseR/", 
+             positions = "./03-phasing\ pipeline/dependencies/SNV.bed", 
+             WCregions = "./03-phasing\ pipeline/dependencies/wc_regions.txt",
+	     exportVCF  =  'spr_output', 
+	     bsGenome  =  'BSgenome.Hsapiens.UCSC.hg38',
+	     pairedEndReads = TRUE)
+
+
